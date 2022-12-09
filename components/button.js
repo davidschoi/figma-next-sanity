780ca9ed7413266b7dcb8fb81e1863908d55ebe4
David Choi <1777321+greatgumz@users.noreply.github.com>
2022-12-09 11:45:54 -0800

    yarn lock update

--- components/button.js
+++ components/button.js
@@ -1,8 +1,32 @@
-function Button({ children, url }) {
+import styled from 'styled-components';
+
+const ButtonStyles = styled.a`
+  background: ${(props) => props.theme.fontColor};
+  color: #fff;
+  display: inline-block;
+  border-radius: 10px;
+  padding: 14px 24px 16px;
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 1;
+  border: 0px;
+  cursor: pointer;
+  user-select: none;
+  transition: transform 200ms ease 0s, background 200ms ease 0s;
+  text-align: center;
+
+  &:hover {
+    transform: translateY(-2px);
+    text-decoration: none;
+}
+  }
+`;
+
+function Button({ children, url, ...props }) {
   return (
-    <a href={url} target="_blank">
+    <ButtonStyles href={url} target="_blank" {...props}>
       {children}
-    </a>
+    </ButtonStyles>
   );
 }
 
--- components/hero.js
+++ components/hero.js
@@ -1,12 +1,58 @@
+import Image from 'next/image';
+import styled from 'styled-components';
 import Button from './button';
 import Wrapper from './wrapper';
+
+const HeroStyles = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .left {
+    width: 540px;
+  }
+
+  h1 {
+    font-size: 64px;
+    font-weight: 500;
+    line-height: 1.15;
+    letter-spacing: -0.03em;
+    margin-top: 0;
+  }
+
+  p {
+    width: 480px;
+    font-size: 18px;
+    line-height: 1.5;
+    letter-spacing: 0.01em;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  .cta {
+    margin-top: 48px;
+  }
+`;
 
 function Hero() {
   return (
     <Wrapper>
-      <h1>David at Figma</h1>
-      <p>Figma is a design platform for teams who build products together. Our vision is to make design accessible to all because we know that nothing great is made alone. We’re a team of makers who celebrate our differences and share a passion for our community. We’re hiring across our global hubs and remotely in the US and Canada. Come join us!</p>
-      <Button url="/David Choi - Resume.pdf">See resume</Button>
+      <HeroStyles>
+        <div className="left">
+          <h1>David at Figma</h1>
+          <div>
+            <p>David is a Developer for the Web Experience team who will build products together. His vision is to make design accessible to all because he knows that nothing great is made alone.</p>
+            <p>He would be thrilled to join a team of makers who celebrate their differences and share a passion for their community. I'm excited to visit the global hubs and work remotely in the U.S. from California.</p>
+            <p>I hope to join Figma!</p>
+          </div>
+          <Button url="/David Choi - Resume.pdf" className="cta">
+            See resume
+          </Button>
+        </div>
+        <Image src="/images/hero.jpg" alt="Hero image" width={540} height={540} priority />
+      </HeroStyles>
     </Wrapper>
   );
 }
